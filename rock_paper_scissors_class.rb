3de0553b33bd303e05987df04ee0b34e800e6403
ryousuke_doi <ryosuke48696869@gmail.com>
2024-01-13 13:04:25 +0900

    [Add] メソッドを二つに分けて処理。 じゃんけんの処理まで

--- rock_paper_scissors_class.rb
+++ rock_paper_scissors_class.rb
@@ -3,6 +3,13 @@ class JankenAttihoi
     puts 'ジャンケン'
     puts "0: グー, 1: チョキ, 2: パー"
     @your_hand_s = gets.to_i
+    if @your_hand_s > 2
+      puts '0~2を選択してください'
+      puts '-----やり直し-----'
+      return initialize
+    else
+      puts 'ポン'
+    end
   end
 
   def rock_paper_scissors
@@ -11,21 +18,14 @@ class JankenAttihoi
     cpu_hand_s = rand(3)
     cpu_hand = hand.sample
 
-    if @your_hand_s < hand.length - 1
-      puts 'ポン'
-      puts "あなたの出した手 : #{your_hand}"
-      puts "CPUの出した手 : #{cpu_hand}"
-    else
-      puts '0~2を選択してください'
-      puts '-----やり直し-----'
-      return rock_paper_scissors
-    end
+    puts "あなたの出した手 : #{your_hand}"
+    puts "CPUの出した手 : #{cpu_hand}"
 
     outcome = @your_hand_s - cpu_hand_s
     if outcome == -1 || outcome == 2
-      result = 'win'
+      @rock_paper_scissors_result = 'win'
     elsif outcome == -2 || outcome == 1
-      result = 'lose'
+      @rock_paper_scissors_result = 'lose'
     else
       puts 'あいこ'
       return rock_paper_scissors
